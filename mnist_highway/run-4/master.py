c1f1db6cebce80bbe4606b19f7a8e6044afd157c
tomard <tomard@usi.ch>
2017-05-09 15:52:33 +0200

    batch_norm

--- mnist_highway/run-4/master.py
+++ mnist_highway/run-4/master.py
@@ -25,11 +25,24 @@ valid_y = torch.from_numpy(valid_y).cuda()
 
 class Highway(nn.Module):
     def __init__(self, fan_in, fan_out, w_init='kaiming_normal', b_init=-2.0):
+        """
+        Fully connected Highway Layer
+        :param fan_in: Number of nodes in the previous layer
+        :param fan_out: Number of nodes in this highway layer
+        :param w_init: Any of the PyTorch's initialization methods
+        :type w_init: String
+        :param b_init: Initialization value for transform gates' bias
+        :type b_init: Constant
+        """
         super(Highway, self).__init__()
+        self.fan_in = fan_in
         # Affine transformation layer and transform gates
         self.linear = nn.Linear(fan_in, fan_out)
         self.transform = nn.Linear(fan_in, fan_out)
         self.batch_norm = nn.BatchNorm1d(fan_out)
+        # The number of the node in the previous layer to which output of each node in this layer is added
+        # Look at forward function for more clarity
+        self.order = range(fan_out)
         # Get weight initialization function
         w_initialization = getattr(nn.init, w_init)
         w_initialization(self.linear.weight)
@@ -37,29 +50,56 @@ class Highway(nn.Module):
         nn.init.constant(self.transform.bias, b_init)
         nn.init.uniform(self.linear.bias)
 
+    def prune(self, nodes):
+        """
+        Create new layers and copy the parameters of the selected nodes
+        :param nodes: list of nodes to retain
+        :type nodes: python List
+        """
+        self.order = nodes
+        # New linear layer
+        linear = nn.Linear(self.fan_in, len(nodes))
+        linear.weight = torch.nn.Parameter(self.linear.weight[torch.LongTensor(nodes)].data)
+        linear.bias = torch.nn.Parameter(self.linear.bias[torch.LongTensor(nodes)].data)
+        self.linear = linear
+        # New transform layer
+        linear = nn.Linear(self.fan_in, len(nodes))
+        linear.weight = torch.nn.Parameter(self.transform.weight[torch.LongTensor(nodes)].data)
+        linear.bias = torch.nn.Parameter(self.transform.bias[torch.LongTensor(nodes)].data)
+        self.transform = linear
+        # New batch normalization layer
+        bn = nn.BatchNorm1d(len(nodes))
+        bn.weight = torch.nn.Parameter(self.batch_norm.weight[torch.LongTensor(nodes)].data)
+        bn.bias = torch.nn.Parameter(self.batch_norm.bias[torch.LongTensor(nodes)].data)
+        bn.running_mean = self.batch_norm.running_weight
+        bn.running_var = self.batch_norm.running_var
+        self.batch_norm = bn
+
     def forward(self, x, train_mode=True):
         h = F.leaky_relu(self.linear(x))
         t = F.sigmoid(self.transform(x))
         self.batch_norm.training = train_mode
-        return self.batch_norm(h * t + (1 - t) * x), t
+        for i in xrange(len(self.order)):
+            x[self.order[i]] = (1 - t[i]) * x[self.order[i]] + h[i] * t[i]
+        return self.batch_norm(x), t
 
 
 class Net(nn.Module):
-    def __init__(self, fan_in=784, fan_out=150):
+    def __init__(self, fan_in=784, fan_out=250):
         super(Net, self).__init__()
         self.linear = nn.Linear(fan_in, fan_out)
         self.highway_layers = []
         self.final = nn.Linear(fan_out, 10)
-        for i in xrange(10):
+        for i in xrange(5):
             self.highway_layers.append(Highway(fan_out, fan_out).cuda())
 
     def forward(self, x, train_mode=True, get_t=False):
         net = F.leaky_relu(self.linear(x))
-        temp = None
-        t_cost = 0
+        temp, t_sum = None, 0
         for layer in self.highway_layers:
-            net,t = layer(net, train_mode)
-            t_cost = torch.sum(torch.max(t, dim=0)[0])
+            net, t = layer(net, train_mode)
+            # Sum of all transform gate for all nodes after taking max value in the batch for each node
+            t_sum = torch.sum(torch.max(t, dim=0)[0])
             if get_t:
                 if temp is None:
                     temp = np.expand_dims(t.numpy(), axis=1)
@@ -69,8 +109,14 @@ class Net(nn.Module):
         if get_t:
             return net, temp.numpy()
         if train_mode:
-            return net, t_cost
+            return net, t_sum
         return net
+
+
+def loss(y, targets):
+    temp = F.softmax(y)
+    l = [-torch.log(temp[i][targets[i].data[0]]) for i in range(y.size(0))]
+    return F.cross_entropy(y, targets), l
 
 network = Net()
 network = network.cuda()
@@ -85,6 +131,15 @@ for epoch in xrange(1, epochs+1):
 
     if epoch > 30:
         optimizer = optim.SGD(network.parameters(), lr=0.01, momentum=0.7, weight_decay=0.0001)
+    if epoch == 50 or epoch == 3:
+        cursor, t_values = 0, 0
+        while cursor < len(train_x):
+            outputs, t_batch = network(Variable(train_x[cursor:min(cursor + batch_size, len(train_x))]), get_t=True)
+            if cursor == 0:
+                t_values = t_batch
+            else:
+                t_values = np.append(t_values, t_batch, axis=0)
+        print t_values.shape
     cursor = 0
     while cursor < len(train_x):
         optimizer.zero_grad()
