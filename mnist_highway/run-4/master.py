11e1ae12b95422cfe281b0ec98d2816cded9e3f1
tomard <tomard@usi.ch>
2017-05-10 12:58:51 +0200

    testing pruning code for bugs

--- mnist_highway/run-4/master.py
+++ mnist_highway/run-4/master.py
@@ -177,7 +177,7 @@ for epoch in xrange(1, epochs + 1):
         optimizer.zero_grad()
         outputs, t_cost = network(Variable(train_x[cursor:min(cursor + batch_size, len(train_x))]))
         if epoch > 10:
-            loss = criterion(outputs, Variable(train_y[cursor:min(cursor + batch_size, len(train_x))])) + 0.04 * t_cost
+            loss = criterion(outputs, Variable(train_y[cursor:min(cursor + batch_size, len(train_x))])) + 0.01 * t_cost
         else:
             loss = criterion(outputs, Variable(train_y[cursor:min(cursor + batch_size, len(train_x))]))
         loss.backward()
